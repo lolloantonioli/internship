89bf9844ba00755c46839bf1a5aafb0085a9ecd7
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2026-03-19 10:29:56 +0100

    Modified gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,7 @@ import java.util.*
 
 plugins {
     application
-    alias(libs.plugins.multiJvmTesting)
     alias(libs.plugins.kotlin.jvm)
-}
-
-multiJvm {
-    jvmVersionForCompilation.set(21)
 }
 
 repositories {
@@ -23,41 +18,11 @@ dependencies {
     }
 }
 
-// Heap size estimation for batches
-val maxHeap: Long? by project
-val heap: Long =
-    maxHeap ?: if (System.getProperty("os.name").lowercase().contains("linux")) {
-        try {
-            val memAvailableKb = ProcessBuilder("bash", "-c", "grep MemAvailable /proc/meminfo | grep -o '[0-9]*'")
-                .redirectErrorStream(true)
-                .start()
-                .inputStream
-                .bufferedReader()
-                .use { it.readText().trim() }
-                .toLong() / 1024
-            println("Detected ${memAvailableKb}MB RAM available.")
-            memAvailableKb * 9 / 10
-        } catch (e: Exception) {
-            println("Could not detect RAM, falling back to default. Error: ${e.message}")
-            14 * 1024L
-        }
-    } else {
-        14 * 1024L
-    }
-
-val taskSizeFromProject: Int? by project
-val taskSize = taskSizeFromProject ?: 512
-val threadCount = maxOf(1, minOf(Runtime.getRuntime().availableProcessors(), heap.toInt() / taskSize))
-val alchemistGroupBatch = "Run batch simulations"
 val alchemistGroupGraphic = "Run graphic simulations with Alchemist"
 
 val runAllGraphic by tasks.register<DefaultTask>("runAllGraphic") {
     group = alchemistGroupGraphic
     description = "Launches all simulations with the graphic subsystem enabled"
-}
-val runAllBatch by tasks.register<DefaultTask>("runAllBatch") {
-    group = alchemistGroupBatch
-    description = "Launches all experiments"
 }
 
 fun String.capitalizeString(): String = this.replaceFirstChar {
@@ -82,7 +47,7 @@ File(rootProject.rootDir.path + "/src/main/yaml")
             args("run", it.absolutePath)
             javaLauncher.set(
                 javaToolchains.launcherFor {
-                    languageVersion.set(JavaLanguageVersion.of(multiJvm.latestJava))
+                    languageVersion.set(JavaLanguageVersion.of(21))
                 },
             )
             if (System.getenv("CI") == "true") {
@@ -105,11 +70,4 @@ File(rootProject.rootDir.path + "/src/main/yaml")
             )
         }
         runAllGraphic.dependsOn(graphic)
-        val batch by basetask("run${capitalizedName}Batch") {
-            group = alchemistGroupBatch
-            description = "Launches batch experiments for $capitalizedName"
-            maxHeapSize = "${minOf(heap.toInt(), Runtime.getRuntime().availableProcessors() * taskSize)}m"
-            File("data").mkdirs()
-        }
-        runAllBatch.dependsOn(batch)
     }
